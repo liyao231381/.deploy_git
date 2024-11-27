0480b81488f02ea67570c28f83536148c57ffd17
liyao231381 <2313814221@qq.com>
2024-11-28 06:12:50 +0800

    Site updated: 2024-11-28 06:12:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/28/我是男的李瑶瑶/","2024/11/27/hello-world/","2024/11/25/这是一篇新的博文-1/"];function toRandomPost(){
+var posts=["2024/11/28/我是男的李瑶瑶/","2024/11/28/【每日早报】-2024-11-28 - 星期四/","2024/11/27/hello-world/","2024/11/25/这是一篇新的博文-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
